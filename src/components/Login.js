960345c8d1c3bcada7164ac198c93741a8b61644
Rohit Kumar <rohitkumar703314@gmail.com>
2022-01-18 22:17:01 +0530

    Adding Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-function Login() {
+
+const Login = (props) => {
+    const [credentials, setCredentials] = useState({ email: "", password: "" })
+    let navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: credentials.email, password: credentials.password })
+        });
+        const json = await response.json()
+        console.log(json);
+        if (json.success) {
+            // Save the auth token and redirect
+            console.log('Navigate')
+            localStorage.setItem('token', json.authtoken);
+            navigate("/", { replace: true });
+        }
+        else {
+            alert("Invalid credentials");
+        }
+    }
+
+    const onChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
+
     return (
-        <div>
-            This is login.
+        <div className='container my-5'>
+            <form onSubmit={handleSubmit} action='/' >
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label"><h4>Email address</h4> </label>
+                    <input type="email" className="form-control" value={credentials.email} onChange={onChange} id="email" name="email" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label"><h4>Password</h4></label>
+                    <input type="password" className="form-control" value={credentials.password} onChange={onChange} name="password" id="password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     )
 }
