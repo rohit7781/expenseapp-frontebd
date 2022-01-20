c104d647feba28de07f45858412431ac33642f54
Rohit Kumar <rohitkumar703314@gmail.com>
2022-01-20 13:14:00 +0530

    Fetching notes

---
+++ src/components/Noteitem.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext';
+
+function Noteitem(props) {
+    const context = useContext(noteContext);
+    const { deleteNote } = context;
+    const { note, updateNote } = props;
+    const now = note.date.toString();
+    return (
+        <>
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <td style={{ width: "30%" }} >{note.title}</td>
+                        <td className='text-center' style={{ width: "15%" }}>{note.amount}</td>
+                        <td style={{ width: "30%" }}>{note.tag}</td>
+                        <td className='text-center' style={{ width: "10%" }}>{now.slice(0, 10)}</td>
+                        <td className='text-center'><i className="fas fa-trash-alt " onClick={() => { deleteNote(note._id); props.showAlert("Deleted Successfully", 'success') }} ></i>
+                            <i className="fas fa-edit mx-3 " onClick={() => { updateNote(note) }}></i>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </>
+    )
+}
+
+export default Noteitem
